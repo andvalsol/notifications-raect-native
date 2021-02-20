fdcbe59105a7733c1e0bb440201bae6d70d7d9a1
Carlos Valverde Solera <and_val9@hotmail.com>
2021-02-19 19:17:45 -0600

    Created local notifications

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import * as Notifications from "expo-notifications"
+import * as Permissions from "expo-permissions"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    useEffect(() => {
+        // For iOS
+        Permissions.getAsync(Permissions.NOTIFICATIONS)
+            .then((response) => {
+                if (response.status !== "granted") {
+                    return Permissions.askAsync(Permissions.NOTIFICATIONS)
+                }
+
+                return response
+            }).then((response) => {
+                if (response.status !== "granted") {
+                    // TODO make an alert message indicating to the user that the permission is needed
+                }
+        })
+    })
+
+  const triggerLocalNotification = () => {
+    // Trigger local notification
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the body of the notification"
+      },
+      trigger: {
+        seconds: 10
+      }
+    })
+  }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Button
+              title="Trigger notification"
+              onPress={triggerLocalNotification}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
